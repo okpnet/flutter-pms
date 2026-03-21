bb17c5e2740b31ad4f206d2bc3a48121d92c0750
umai-beer <umai-beer@hotmail.co.jp>
2026-03-21 23:28:16 +0900

    test成功

--- pms_graphql_lib/test_hasura/test/client_provider_test.dart
+++ pms_graphql_lib/test_hasura/test/client_provider_test.dart
@@ -109,4 +109,44 @@ void main() {
     debugPrint(result.first.exception.toString());
     expect(result.first.hasException, false);
   });
+
+  test('should execute multiple insert successfully', () async {
+    final insertModel1 = StaffEditModel(
+      name: 'multi test4',
+      code: 'ABC4',
+      info_staff_id: '6d81899b-3ee5-42d3-94fd-915640ac33b6',
+    );
+    final insertModel2 = StaffEditModel(
+      name: 'multi test5',
+      code: 'ABC5',
+      info_staff_id: 'a0ef1418-0dc4-4bc0-a04e-23c5429bcb98',
+    );
+    final insertModel3 = StaffEditModel(
+      name: 'multi test6',
+      code: 'ABC6',
+      info_staff_id: 'daf315bd-cc3c-4096-a977-a6a934645cab',
+    );
+    final collection = GraphQLConverterCollection([
+      CollectionItem<StaffEditModel>(StaffEditModelConverter()),
+    ]);
+    final provider = GraphQLClientProvider(
+      endpoint,
+      headers: {
+        'content-type': 'application/json',
+        'x-hasura-admin-secret': 'admin',
+      },
+      timeLimit: 10,
+      converterCollection: collection,
+      isHasura: true,
+      logger: Logger(),
+    );
+    final result = await provider.save([
+      insertModel1,
+      insertModel2,
+      insertModel3,
+    ]);
+    debugPrint(result.map((e) => e.data).toString());
+    debugPrint(result.first.exception.toString());
+    expect(result.first.hasException, false);
+  });
 }
