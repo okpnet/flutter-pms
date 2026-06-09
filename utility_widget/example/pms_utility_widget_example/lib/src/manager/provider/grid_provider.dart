f934fa88f6afe43f2bfbaa5e3a7eb527d82d1975
hayato <umai-beer@hotmail.co.jp>
2026-06-09 17:42:38 +0900

    競合解決

--- utility_widget/example/pms_utility_widget_example/lib/src/manager/provider/grid_provider.dart
+++ utility_widget/example/pms_utility_widget_example/lib/src/manager/provider/grid_provider.dart
@@ -2,18 +2,15 @@ import 'dart:async';
 
 import 'package:trina_grid/trina_grid.dart';
 import 'package:utility_widget/utiritiy_widget.dart';
-import 'package:utility_widget_example/constant/demo/demo_trree_reder_service.dart';
 import 'package:utility_widget_example/constant/results/result.dart';
 import 'package:utility_widget_example/extensions/pluto_grid/trina_column_extension.dart';
 import 'package:utility_widget_example/extensions/pluto_grid/trina_row_extension.dart';
 import 'package:utility_widget_example/src/condition_pipeline/condition/field_operator.dart';
 import 'package:utility_widget_example/src/condition_pipeline/condition/search_condition.dart';
-import 'package:utility_widget_example/src/manager/compare/comare.dart';
 import 'package:utility_widget_example/src/manager/model/summary_data.dart';
 import 'package:utility_widget_example/src/manager/service/pms_repository_service.dart';
 import 'package:utility_widget_example/src/manager/state/grid_state.dart';
 import 'package:utility_widget_example/src/manager/state/pms_state.dart';
-import 'package:utility_widget_example/src/manager/state/mixin/grid_summary_state.dart';
 import 'package:utility_widget_example/src/ui/pms_widget_state.dart';
 
 part 'pagenation_provider.dart';
@@ -26,9 +23,6 @@ abstract class IGridStateManagerOfTrinaGrid {
 
   ///
   TrinaGridStateManager get stateManager;
-
-  ///
-  void setGridStatemnager(TrinaGridStateManager trinaGridStateManager);
 }
 
 ///ツリー
